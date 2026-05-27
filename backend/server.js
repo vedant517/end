3cfc44a808c2e808e0924f5df849bbda73b2fe6c
vedant517 <your-github-email@example.com>
2026-05-27 10:36:09 +0530

    Add http://www.sheetalya.com to CORS allowlist

--- backend/server.js
+++ backend/server.js
@@ -52,7 +52,8 @@ const configuredOrigins = [
   "http://127.0.0.1:5173",
   "http://localhost:3000",
   "http://127.0.0.1:3000",
-  "https://www.sheetalya.com"
+  "https://www.sheetalya.com",
+  "http://www.sheetalya.com"
 ]
   .flatMap((value) => (value || "").split(","))
   .map(normalizeOrigin)
